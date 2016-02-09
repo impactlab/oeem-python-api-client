2bf880c0928ba94c44d6d21c814de1e177f88d04
hunterowens <owens.hunter@gmail.com>
2016-02-09 16:15:30 -0600

    basic setup of package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='oee_client',
+      version='0.1',
+      description='An OEE datastore API client',
+      url='http://github.com/impactlab/oee-python-api-client',
+      author='Hunter Owesn',
+      author_email='hunter@theimpactlab.co',
+      license='MIT',
+      packages=['oee_client'],
+      zip_safe=False)
