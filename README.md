82a85fe0d16e6d2183f74c194864a4f836418915
hunterowens <owens.hunter@gmail.com>
2016-02-10 14:57:34 -0600

    Auth.  Fixes #13

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # oeem-python-api-client
 Python client for the oeem datastore
+
+## Auth
+
+Set the DATASTORE_ACCESS_TOKEN environment variable. 
--- oee_client/auth.py
+++ oee_client/auth.py
@@ -1,3 +1,6 @@
+import os
 
 def auth_token():
-    pass
+    token = os.environ.get("DATASTORE_ACCESS_TOKEN", 'uFxwXUtDFz2a8ha2wXMxBqD7WTW623fpy7pbNpmq')
+    return token 
+    
