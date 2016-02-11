7b42f08a5648d60d49a5b9964ed3e9e39a5c54b8
hunterowens <owens.hunter@gmail.com>
2016-02-11 11:46:03 -0600

    url

--- oee_client/auth.py
+++ oee_client/auth.py
@@ -3,4 +3,10 @@ import os
 def auth_token():
     token = os.environ.get("DATASTORE_ACCESS_TOKEN", 'uFxwXUtDFz2a8ha2wXMxBqD7WTW623fpy7pbNpmq')
     return token 
+
+def datastore_url():
+    """Get the datastore URL
+    """
+    url = os.environ.get("DATASTORE_URL", "test-datastore.openeemeter.org")
+    return url
     
