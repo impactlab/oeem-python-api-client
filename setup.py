0fc7325c7fa42afcc2076f5ee36e3e79b3a448ad
hunterowens <owens.hunter@gmail.com>
2016-02-18 13:06:10 -0600

    tests

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup
 
-setup(name='oee_client',
+setup(name='oeem_uploader',
       version='0.1',
       description='An OEE datastore API client',
-      url='http://github.com/impactlab/oee-python-api-client',
-      author='Hunter Owesn',
+      url='http://github.com/impactlab/oeem_uploader',
+      author='Hunter Owens',
       author_email='hunter@theimpactlab.co',
       license='MIT',
-      packages=['oee_client'],
-      zip_safe=False)
+      packages=['oeem_uploader'],
+      zip_safe=False,
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      )
