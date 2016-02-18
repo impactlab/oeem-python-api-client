b4634a55d8d85eea60d3749969fa56fabd6df9cc
hunterowens <owens.hunter@gmail.com>
2016-02-18 14:54:47 -0600

    a basic request override. Fixes #14

--- oeem_uploader/request.py
+++ oeem_uploader/request.py
@@ -1,4 +1,14 @@
-import request
+import requests
+from .auth import auth_token, datastore_url
 
 class Request:
-    pass
+    def __init__(self):
+        self.token = auth_token()
+        self.headers = {'Authorization': "Bearer " + self.token}
+        self.url = datastore_url()
+
+    def get(self, resource): 
+        """Makes a get request to the resource
+        """
+        r = requests.get('http://' + self.url + '/api/v1/'+ resource, headers=self.headers)
+        return r 
